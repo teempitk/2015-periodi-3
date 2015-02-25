46560a93d8dfb719c065019b153881bdcbf10798
Teemu Pitkänen <teemu.pitkanen@helsinki.fi>
2015-02-25 17:49:09 +0200

    inverse BWT almost ready

--- tiralabra/src/Tranforms/BurrowsWheeler.java
+++ tiralabra/src/Tranforms/BurrowsWheeler.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 /**
  * BWT on myös yksi datalle tehtävä muunnos, joka ei vaikuta tiedoston kokoon,
@@ -15,15 +20,23 @@ import java.nio.file.Paths;
  */
 public class BurrowsWheeler {
 
+    /**
+     * Bytearray, johon syötetiedosto luetaan.
+     */
     private static byte[] data;
-
+    /**
+     * Sisältää "kaikki datan sanat", data.length numeroa, jotka kukin vastaavat
+     * kyseisestä datan indeksistä alkavaa koko datan pituista sanaa (mukana
+     * kierto alkuun, eli jokainen "sana" sisältää datan kaikki merkit). Näiden
+     * järjestäminen on BWT:n merkittävä vaihe.
+     */
     private static int[] lines;
 
     /*
-    Metodi paikalliseen pikatestailuun
-    */
-  //  public static void main(String[] args) throws IOException {
-   //     transform("sampleFiles/abc.txt","sampleFiles/bbb.txt");
+     Metodi paikalliseen pikatestailuun
+     */
+    //  public static void main(String[] args) throws IOException {
+    //     transform("sampleFiles/abc.txt","sampleFiles/bbb.txt");
 //        byte A = (byte) 'A';
 //        byte B = (byte) 'B';
 //        byte C = (byte) 'C';
@@ -43,30 +56,32 @@ public class BurrowsWheeler {
 //        for (int i = 0; i < res.length; i++) {
 //            System.out.print(res[i] + ", ");
 //        }
-        // oikea tulos : 4,1,3,0,5,2
-  //  }
-
+    // oikea tulos : 4,1,3,0,5,2
+    //  }
     public static void transform(String inputFile, String outputFile) throws IOException {
-        System.out.println("Burrows-Wheeler -transform started");
-        Path pathToOriginalData = Paths.get(inputFile);
-        data = Files.readAllBytes(pathToOriginalData);
+        readInputData(inputFile);
         byte[] transformedData = new byte[data.length];
         lines = new int[data.length];
         int lastByteInOriginalDataPointer;
-        for(int i=0; i<data.length; i++){
-            lines[i]=i;
+        for (int i = 0; i < data.length; i++) {
+            lines[i] = i;
         }
         lines = quickSortLines(lines, 0);
         for (int i = 0; i < data.length; i++) {
-            //transformedData[i] = data[lines[(i - 1 + data.length) % data.length]]; //LUULTAVASTI VIRHEEN SYY
-            transformedData[i] = data[(lines[i]-1+data.length)%data.length];
-            if(lines[i]==0){
+            transformedData[i] = data[(lines[i] - 1 + data.length) % data.length];
+            if (lines[i] == 0) {
                 lastByteInOriginalDataPointer = i;
             }
         }
         FileOutputStream outStream = new FileOutputStream(new File(outputFile));
         outStream.write(transformedData);
         outStream.close();
+    }
+
+    private static void readInputData(String inputFile) throws IOException {
+        System.out.println("Burrows-Wheeler -transform started");
+        Path pathToOriginalData = Paths.get(inputFile);
+        data = Files.readAllBytes(pathToOriginalData);
     }
 
     /**
@@ -141,4 +156,60 @@ public class BurrowsWheeler {
         return strings;
     }
 
+    public static void inverseTransform(String inputFile, String outputFile) throws IOException {
+        
+        int indexOfLastCharInOriginalData = readIndexOfLast();
+        
+        readInputData(inputFile);
+        byte[] sorted = Arrays.copyOf(data, data.length);
+        Arrays.sort(sorted);
+
+        // Tässä osassa taulukon i:nnessä indeksissä on lista, jossa on i:nnen tavun (esim 1. tavu on 00000001) esiintymien indeksit muunnetussa datassa. Tätä tarvitaan myöhemmin.
+        LinkedList<Integer>[] arr = new LinkedList[256];
+        for (int i = 0; i < data.length; i++) {
+            byte b = data[i];
+            int index = b;
+            if (index < 0) {
+                index += 256;
+            }
+            LinkedList list = arr[index];
+            if (list == null) {
+                list = new LinkedList();
+                arr[index] = list;
+            }
+            list.addLast(i);
+        }
+
+        //Seuraava osa selvittää kullekin järjestetyn taulukon tavulle edeltävän tavun sijainnin muunnetussa datassa.
+        int[] previousLetters = new int[data.length];
+        for (int i = 0; i < sorted.length; i++) {
+            byte b = sorted[i];
+            int index = b;
+            if (index < 0) {
+                index += 256;
+            }
+            LinkedList<Integer> list = arr[index];
+            previousLetters[i] = list.getFirst();
+            list.remove();
+        }
+        
+        // Sitten vain luetaan alkuperäinen data edellä luodun taulukon avulla
+        byte [] originalData = new byte[data.length];
+        int indexOfCurr = indexOfLastCharInOriginalData;
+        for (int i=0; i<data.length; i++){
+            originalData[data.length-i]=data[indexOfCurr];
+            indexOfCurr = previousLetters[indexOfCurr];
+        }
+        
+        FileOutputStream outStream = new FileOutputStream(new File(outputFile));
+        outStream.write(originalData);
+        outStream.close();
+        
+        
+        
+    }
+
+    private static int readIndexOfLast() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
